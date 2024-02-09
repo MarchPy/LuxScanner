4805b0ae1fa1bfce4450350a5b28de3b8f79a3ce
MarchPy <joaomarchiori.jopa@gmail.com>
2024-02-08 23:25:39 -0300

    Update

--- LuxScanner.py
+++ LuxScanner.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import pandas as pd
 import yfinance as yf
+from sys import argv
 from datetime import datetime
 from rich.console import Console
 
@@ -10,10 +11,12 @@ from rich.console import Console
 console = Console()
 
 
+# Função para retornar o horário atual
 def time():
     return datetime.now().strftime(format='%H:%M:%S')
 
 
+# Função para retornar a data atual
 def date():
     return datetime.now().strftime(format='%d-%m-%Y')
 
@@ -28,7 +31,7 @@ class LuxScanner:
         except FileNotFoundError:
             pass
 
-    def colect_data_from_json_file(self):
+    def colect_data_from_json_file(self, save_file=False):
         # Resultado final
         data = []
 
@@ -47,71 +50,108 @@ class LuxScanner:
                         interval=self.data_tickers['timeframe']['interval']
                     )
 
+                    # Verificando se o DataFrame não está vázio para realizar o calculo de indicadores
                     if not df_yf.empty:
-                        result = self.strategy(df=df_yf)
-                        if result is not None:
-                            close = round(df_yf['Close'].iloc[-1], 2)
-                            crossover, rsi, volume, check_volume = result
-                            data.append([ticker, close, sector_name, crossover, rsi, volume, check_volume])
+                        # Coletando alguns indicadores 
+                        rsi = self.rsi(df=df_yf)
+                        crossover = self.crossover(df=df_yf)
+                        cycle = self.cycle(df=df_yf)
+                        
+                        data.append([ticker, sector_name, crossover, cycle, rsi])
+        
+        # Definindo o DataFrame final
+        columns = ['Ativo', 'Setor', 'Cruzamento', 'Ciclo', 'RSI']
+        df_final = pd.DataFrame(data=data, columns=columns)
+        df_final = df_final[df_final['Cruzamento'] != '-']
+        
+        # Exibição de resultado
+        os.system(command='cls' if os.name == 'nt' else 'clear')
+        console.print(f"[[bold white]{time()}[/]] -> [[bold green]Resultado[/]]:\n\n{str(df_final) if not df_final.empty else "[red]Tabela vázia (Nemhuma oportunidade de investimento encontrada)[/]"}\n")
+        
+        if save_file:
+            self.save_as_file(df=df_final)
 
-        self.save_as_file(data=data)
+    def rsi(self, df: pd.DataFrame):
+        # Calculando RSI
+        period = self.data_tickers['rsi']
 
-    def strategy(self, df: pd.DataFrame):
-        volume = int(df['Volume'].tail(n=21).mean())
-        if volume > self.data_tickers['volume >']:
-            
-            # Calculando RSI
-            period = self.data_tickers['rsi']
+        def rma(x, n, y0):
+            a = (n - 1) / n
+            ak = a**np.arange(len(x) - 1, -1, -1)
+            return np.r_[np.full(n, np.nan), y0, np.cumsum(
+                ak * x) / ak / n + y0 * a**np.arange(1, len(x) + 1)]
 
-            def rma(x, n, y0):
-                a = (n - 1) / n
-                ak = a**np.arange(len(x) - 1, -1, -1)
-                return np.r_[np.full(n, np.nan), y0, np.cumsum(
-                    ak * x) / ak / n + y0 * a**np.arange(1, len(x) + 1)]
+        df['change'] = df['Close'].diff()
+        df['gain'] = df.change.mask(df.change < 0, 0.0)
+        df['loss'] = -df.change.mask(df.change > 0, -0.0)
+        df['avg_gain'] = rma(df.gain[period + 1:].to_numpy(), period, np.nansum(df.gain.to_numpy()[:period + 1]) / period)
+        df['avg_loss'] = rma(df.loss[period + 1:].to_numpy(), period, np.nansum(df.loss.to_numpy()[:period + 1]) / period)
+        df['rs'] = df.avg_gain / df.avg_loss
+        df['rsi'] = 100 - (100 / (1 + df.rs))
+        rsi = round(df['rsi'][-1:].values[0], 2)
+        
+        return rsi
+    
+    def crossover(self, df: pd.DataFrame):
+        # Valores dos periodos
+        short_period = self.data_tickers['crossover']['short_period']
+        long_period = self.data_tickers['crossover']['long_period']
 
-            df['change'] = df['Close'].diff()
-            df['gain'] = df.change.mask(df.change < 0, 0.0)
-            df['loss'] = -df.change.mask(df.change > 0, -0.0)
-            df['avg_gain'] = rma(df.gain[period + 1:].to_numpy(), period, np.nansum(df.gain.to_numpy()[:period + 1]) / period)
-            df['avg_loss'] = rma(df.loss[period + 1:].to_numpy(), period, np.nansum(df.loss.to_numpy()[:period + 1]) / period)
-            df['rs'] = df.avg_gain / df.avg_loss
-            df['rsi'] = 100 - (100 / (1 + df.rs))
-            rsi = round(df['rsi'][-1:].values[0], 2)
+        # Calculando as médias exponencial/não exponencial
+        if self.data_tickers['crossover']['exponential'] == "True":
+            df[f'MM_{short_period}'] = df['Close'].ewm(span=short_period).mean()
+            df[f'MM_{long_period}'] = df['Close'].ewm(span=long_period).mean()
 
-            # Valores dos periodos
-            short_period = self.data_tickers['crossover']['short period']
-            long_period = self.data_tickers['crossover']['long period']
+        else:
+            df[f'MM_{short_period}'] = df['Close'].rolling(window=short_period).mean()
+            df[f'MM_{long_period}'] = df['Close'].rolling(window=long_period).mean()
 
-            # Calculando as médias exponencial/não exponencial
-            if self.data_tickers['crossover']['exponential'] == "True":
-                df[f'MM_{short_period}'] = df['Close'].ewm(span=short_period).mean()
-                df[f'MM_{long_period}'] = df['Close'].ewm(span=long_period).mean()
+        # Identificando cruzamento
+        df['Sinal'] = 0
+        df.loc[df[f'MM_{short_period}'] > df[f'MM_{long_period}'], 'Sinal'] = 1
+        df.loc[df[f'MM_{short_period}'] < df[f'MM_{long_period}'], 'Sinal'] = -1
+        
+        if df['Sinal'].iloc[-1] != df['Sinal'].iloc[-2]:
+            if df['Sinal'].iloc[-1] == 1:
+                return "Cruzamento de alta"
+
+            elif df['Sinal'].iloc[-1] == -1:
+                return "Cruzamento de baixa"
 
             else:
-                df[f'MM_{short_period}'] = df['Close'].rolling(window=short_period).mean()
-                df[f'MM_{long_period}'] = df['Close'].rolling(window=long_period).mean()
-
-            # Identificando cruzamento
-            df['Sinal'] = 0
-            df.loc[df[f'MM_{short_period}'] > df[f'MM_{long_period}'], 'Sinal'] = 1
-            df.loc[df[f'MM_{short_period}'] < df[f'MM_{long_period}'], 'Sinal'] = -1
+                return "-"
             
-            check_volume = 'Sim' if df['Volume'].iloc[-1] > volume else 'Não'
+        else:
+            return "-"
+    
+    def cycle(self, df: pd.DataFrame):
+        short_period = self.data_tickers['cycle']['short_period']
+        long_period = self.data_tickers['cycle']['long_period']
+            
+        # Calculando as médias móveis simples
+        df[f'SMA_{short_period}'] = df['Close'].rolling(window=short_period).mean()
+        df[f'SMA_{long_period}'] = df['Close'].rolling(window=long_period).mean()
 
-            if df['Sinal'].iloc[-1] != df['Sinal'].iloc[-2]:
-                if df['Sinal'].iloc[-1] == 1:
-                    return ["Cruzamento de alta", rsi, volume, check_volume]
+        # Coletando as últimas informações
+        last_close = df['Close'].iloc[-1]
+        last_sma_short = df[f'SMA_{short_period}'].iloc[-1]
+        last_sma_long = df[f'SMA_{long_period}'].iloc[-1]
 
-                elif df['Sinal'].iloc[-1] == -1:
-                    return ["Cruzamento de baixa", rsi, volume, check_volume]
+        # Fazendo o filtro
+        # Lado de compras
+        if last_close > last_sma_short and last_close < last_sma_long and last_sma_short < last_sma_long: return "Fase de recuperação"
+        elif last_close > last_sma_short and last_close > last_sma_long and last_sma_short < last_sma_long: return "Fase de acumulação"
+        elif last_close > last_sma_short and last_close > last_sma_long and last_sma_short > last_sma_long: return "Fase altista"
 
-                else:
-                    return
+        # Lado de vendas
+        elif last_close > last_sma_short and last_close > last_sma_long and last_sma_short > last_sma_long: return "Fase de aviso"
+        elif last_close < last_sma_short and last_close < last_sma_long and last_sma_short > last_sma_long: return "Fase de distribuição"
+        elif last_close < last_sma_short and last_close < last_sma_long and last_sma_short < last_sma_long: return "Fase baixista"
 
-            else:
-                return
-
-    def save_as_file(self, data: list):
+        else: return "-"
+        
+    def save_as_file(self, df: pd.DataFrame):
+        # Criando a pasta onde seram salvos os arquivos
         save_folder = self.data_tickers['save_folder']
         try:
             os.mkdir(path=save_folder)
@@ -119,18 +159,15 @@ class LuxScanner:
         except FileExistsError:
             pass
         
-        columns = ['Ativo', 'Vl. de Fechamento', 'Setor', 'Cruzamento', 'RSI', 'Vol. Méd. 21p', 'Confir. Volume']
-        df = pd.DataFrame(data=data, columns=columns)
-        os.system(command='cls' if os.name == 'nt' else 'clear')
-        console.print(f"[[bold white]{time()}[/]] -> [[bold green]Resultado[/]]:\n\n{df if not df.empty else "[red]Tabela vázia (Nemhuma oportunidade de investimento encontrada)[/]"}\n")
         filename = f'{save_folder}Relatório de opções de investimento ({date()}).xlsx'
         df.to_excel(filename, index=False)
-        console.print(f'[[bold yellow]Arquivo excel gerado com o resultado. ({filename})[/]]')
+        console.print(f'[[bold yellow]Arquivo excel gerado com o resultado. ([white]{filename}[/])[/]]')
+
 
 if __name__ == "__main__":
-    try:
+    try:        
         app = LuxScanner()
-        app.colect_data_from_json_file()
+        app.colect_data_from_json_file(save_file=True if len(argv) > 1 and argv[1] == '-s' else False)
 
     except KeyboardInterrupt:
         console.print('[[italic bold yellow]Processo encerrado pelo usuário!!![/]]')
--- README.md
+++ README.md
@@ -9,32 +9,42 @@ LuxScanner é uma ferramenta avançada em Python para análise de mercado de aç
 O arquivo de configuração, localizado em 'config/tickers.json', é estruturado para permitir uma personalização abrangente. Os principais elementos incluem:
 
 1. **Setores e Ativos:**
+
    - Defina setores de interesse, cada um com um nome, status de coleta e uma lista de ativos associados.
    - Controle se a coleta de dados é permitida para cada setor, ajustando o status para "True" ou "False".
-   
 2. **Intervalo de Tempo:**
+
    - Configure o intervalo de tempo para coleta de dados, especificando o período e o intervalo desejados.
-
 3. **Estratégia RSI:**
-   - Ajuste o período para o cálculo do RSI (Relative Strength Index) conforme suas preferências.
 
+   - Ajuste o período para o cálculo do RSI (Relative Strength Index) conforme suas preferências.
 4. **Cruzamento de Médias:**
+
    - Configure os períodos curto e longo para o cruzamento de médias móveis.
    - Escolha entre médias exponenciais ou não exponenciais.
 
 **Funcionalidades Principais:**
 
 - **Coleta de Dados Dinâmica:**
-  - A ferramenta coleta dinamicamente dados do Yahoo Finance para os ativos especificados no arquivo de configuração.
-  
-- **Análise de Estratégia:**
-  - Utiliza uma estratégia de investimento que considera cruzamento de médias móveis, RSI e análise de volume para identificar oportunidades de investimento.
- 
-- **Limiares Configuráveis:**
-  - Estabeleça limiares para o volume, personalizando a análise com base em suas preferências. 
 
+  - A ferramenta coleta dinamicamente dados do Yahoo Finance para os ativos especificados print(argv)no arquivo de configuração.
+- **Análise de Estratégia:**
+
+  - Utiliza uma estratégia de investimento que considera cruzamento de médias móveis, RSI e análise de volume para identificar oportunidades de investimento.
+- **Limiares Configuráveis:**
+
+  - Estabeleça limiares para o volume, personalizando a análise com base em suas preferências.
 - **Relatórios Detalhados:**
+
   - Os resultados da análise são apresentados em um relatório Excel, destacando cruzamentos de médias, valores de RSI e volumes médios ao longo de um período específico.
+
+**Para salvar o resultado em um arquivo execute o programa utilizando o parâmetro** `-s`
+
+Exemplo: 
+
+```bash
+python3 LuxScanner.py -s
+```
 
 Essa abordagem personalizável e abrangente permite que investidores ajustem facilmente a estratégia de acordo com suas preferências e objetivos financeiros. Experimente o LuxScanner para aprimorar suas decisões de investimento com análises técnicas avançadas.
 
@@ -45,18 +55,19 @@ O Fundamentus Scraper é uma ferramenta Python que simplifica a análise fundame
 **Características Principais:**
 
 - **Lista de Ações Padrão:**
+
   - O código vem pré-configurado com uma lista abrangente de tickers de ações brasileiras, abrangendo uma variedade de setores.
-  
 - **Coleta Dinâmica de Dados:**
+
   - A ferramenta acessa o site Fundamentus para cada ticker, coletando informações cruciais sobre a empresa.
-  
 - **Tratamento de Dados:**
+
   - Os dados coletados são organizados em um DataFrame do Pandas, tornando-os fáceis de manipular e analisar.
-  
 - **Formatação Adequada:**
+
   - A ferramenta realiza a conversão e formatação adequada de dados, garantindo que os valores estejam prontos para análise.
-  
 - **Exportação para Excel:**
+
   - Os resultados finais são exportados para um arquivo Excel chamado "Fundamentus.xlsx", fornecendo uma visão consolidada e organizada das métricas fundamentais das ações.
 
 **Como Usar:**
